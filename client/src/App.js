de8ba4a49118b526b762d690348888afe3443938
kgobin2000 <44628334+kgobin2000@users.noreply.github.com>
2022-01-13 00:24:07 -0500

    yea

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import Home from "home";
 
 import "./App.css";
 
